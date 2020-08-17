90c4931c75052233afafa3c7b5a356a14361890c
Gabi Pires <gabrielatrindadepires@gmail.com>
2020-08-17 18:30:36 -0300

    ajustes finais

--- src/server.js
+++ src/server.js
@@ -24,6 +24,10 @@ server
 //receber os dados do req body, invés de receber pelo req.query (mostra os dados na url)
 .use(express.urlencoded({ extended:true }))
 
+
+
+
+
 // estamos configurando o servidor para que ele enxergue a pasta public, arquivos estáticos
 .use(express.static("public")) 
 
@@ -33,5 +37,12 @@ server
 .get("/give-classes", pageGiveClasses)
 .post("/save-classes", saveClasses)
 
+
+
+
 //Start do Servidor
 .listen(5500)
+
+
+
+
